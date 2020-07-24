a84a59c8450d9d150d48d38a3e8158fc9419acdd
WordsPerMinute <git@coreyhodge.net>
2020-07-24 17:38:30 -0600

    added a test post

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,11 @@
+// import React, { Fragment } from "react"
+//
+// const BioContent = () => (
+//   <Fragment>
+//     Words by <a href="https://aisha.codes/">Aisha Blake</a>.
+//     <br />
+//     I'm a speaker, teacher, and software engineer! I can't spell.
+//   </Fragment>
+// )
+//
+// export default BioContent
